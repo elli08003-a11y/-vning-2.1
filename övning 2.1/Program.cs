4be8a88694c5efd94cdfa02f238f8c6411cd9782
elli08003 <elli08003@E-PW04SM2Y>
2025-09-17 12:55:26 +0200

    Add project files.

---
+++ övning 2.1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace övning2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vad är din favoritbok");
+            string x = Console.ReadLine();
+            Console.WriteLine("Din favoritbok är " + x);
+        }
+    }
+}
